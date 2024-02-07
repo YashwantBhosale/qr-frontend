9f8c8c50e23706ad0c2d3e241fd30716ed783aae
Yashwant Bhosale <82835383+YashwantBhosale@users.noreply.github.com>
2024-02-08 04:03:17 +0530

    Update script.js

--- script.js
+++ script.js
@@ -14,7 +14,7 @@ document.addEventListener('DOMContentLoaded', function () {
       }
   
       try {
-        const response = await fetch('http://localhost:3000/generate-qr', {
+        const response = await fetch('https://qr-backend-tau.vercel.app/generate-qr', {
           method: 'POST',
           headers: {
             'Content-Type': 'application/json'
@@ -42,7 +42,7 @@ document.addEventListener('DOMContentLoaded', function () {
   });
   
   function saveURLToFile(url) {
-    fetch('http://localhost:3000/save-url', {
+    fetch('https://qr-backend-tau.vercel.app/save-url', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json'
